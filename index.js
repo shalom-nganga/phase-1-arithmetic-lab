ffbdbe4fb6188b29b05f4781f16cd79712ce769b
shalom-nganga <minnomacharia@gmail.com>
2023-10-06 08:31:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1=31;
+let num2=2;
+let multiply=num1 * num2;
+let random=Math.floor(Math.random()*10)+1;
 
+
+let num3=4;
+let num4=20;
+let mod=num3 % num4;
+
+
+
+
+let max=Math.max(9,9,6,9,20)
 // Write your code here
